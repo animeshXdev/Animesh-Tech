36f9de3406963e031fce4b9a6551290ca2d6b1c4
animeshxdev <vegan.athelete.animesh@gmail.com>
2025-06-26 08:51:56 +0530

    change in link-preview.tsx in /components/ui

--- src/components/ui/link-preview.tsx
+++ src/components/ui/link-preview.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import * as HoverCardPrimitive from "@radix-ui/react-hover-card";
 
